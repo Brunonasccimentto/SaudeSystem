01169a2bee528a1f4951e198939b202be6b5cdb2
Bruno Rodrigues <brunonasccimentto@gmail.com>
2025-04-12 22:40:06 -0300

    feat: Adicionar migração inicial para a tabela de médicos

---
+++ SistemaSaude/Migrations/20250401202054_initalMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaSaude.Migrations
+{
+    /// <inheritdoc />
+    public partial class initalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    CRM = table.Column<string>(type: "TEXT", maxLength: 13, nullable: false),
+                    Roles = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.CRM);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicos");
+        }
+    }
+}
