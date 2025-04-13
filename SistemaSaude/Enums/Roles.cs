8174cbfb02636fe2639d4ada54c98d5ad2eeca9e
Bruno Rodrigues <brunonasccimentto@gmail.com>
2025-04-12 22:38:09 -0300

    feat: Adicionar enum de Papéis para profissionais de saúde

---
+++ SistemaSaude/Enums/Roles.cs
@@ -0,0 +1,16 @@
+namespace SistemaSaude.Enums
+{
+    public enum Roles
+    {
+        Cardiologista,
+        Endocrionogista,
+        Psiquiatra,
+        Pediatra,
+        Demartologista,
+        Geriatra,
+        Angiologista,
+        Ginecologista,
+        Urologista,
+        ClinicoGeral
+    }
+}
